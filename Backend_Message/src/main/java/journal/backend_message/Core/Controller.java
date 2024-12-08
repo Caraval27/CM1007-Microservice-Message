377c00de3bced22d54631d160c42ed5d414d40a6
Caraval27 <bettypori@gmail.com>
2024-12-08 02:43:14 +0100

    Added url for cbh cloud in controller

--- Backend_Message/src/main/java/journal/backend_message/Core/Controller.java
+++ Backend_Message/src/main/java/journal/backend_message/Core/Controller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:3000", "https://journal-app-frontend.app.cloud.cbh.kth.se:3000"})
 @RestController
 public class Controller {
     @Autowired
