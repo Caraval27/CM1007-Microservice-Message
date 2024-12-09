fca41e1fa428c33dc244ada3598d0aa1e6ed21c4
Caraval27 <bettypori@gmail.com>
2024-12-09 02:51:06 +0100

    changed feign client url

--- Backend_Message/src/main/java/journal/backend_message/Core/UserServiceClient.java
+++ Backend_Message/src/main/java/journal/backend_message/Core/UserServiceClient.java
@@ -5,7 +5,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "user-service", url = "http://localhost:8082")
+//@FeignClient(name = "user-service", url = "http://localhost:8082")
+@FeignClient(name = "user-service", url = "https://journal-app-user.app.cloud.cbh.kth.se/")
 public interface UserServiceClient {
     @GetMapping("/get_user_by_id")
     User getUserById(@RequestParam String id);
